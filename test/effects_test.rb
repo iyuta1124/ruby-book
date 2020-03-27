7b349289ea42fca1fe6cc7d68cad96604f747c46
iyuta1124 <iyuta1124@gmail.com>
2020-03-27 23:12:26 +0900

    ワードシンセサイザー終了

--- test/effects_test.rb
+++ test/effects_test.rb
@@ -8,10 +8,18 @@ class EffectsTest < Minitest::Test
   end
 
   def test_echo
-    effect = Effects.echo(2)
-    assert_equal 'RRuubbyy iiss ffuunn!!', effect.call('Ruby is fun!')
+   effect = Effects.echo(2)
+    assert_equal 'RRuubbyy iiss ffuunn!!', effect.call('Ruby is fun!') 
 
     effect = Effects.echo(3)
     assert_equal 'RRRuuubbbyyy iiisss fffuuunnn!!!', effect.call('Ruby is fun!')
+  end
+
+  def test_loud
+    effect = Effects.loud(2)
+    assert_equal 'RUBY!! IS!! FUN!!!', effect.call('Ruby is fun!')
+
+    effect = Effects.loud(3)
+    assert_equal 'RUBY!!! IS!!! FUN!!!!', effect.call('Ruby is fun!')
   end
 end
--- test/word_synth_test.rb
+++ test/word_synth_test.rb
@@ -3,8 +3,23 @@ require './lib/word_synth'
 require './lib/effects'
 
 class WordsynthTest < Minitest::Test
-  def test_play
-    assert Wordsynth
-    assert Effects
+  def test_play_without_effects
+    synth = Wordsynth.new
+    assert_equal 'Ruby is fun!', synth.play('Ruby is fun!')
+  end
+
+  def test_play_with_reverse
+    synth = Wordsynth.new
+    synth.add_effect(Effects.reverse)
+    assert_equal 'ybuR si !nuf', synth.play('Ruby is fun!')
+  end
+
+  def test_play_with_many_effects
+    synth = Wordsynth.new
+    synth.add_effect(Effects.echo(2))
+    synth.add_effect(Effects.loud(3))
+    synth.add_effect(Effects.reverse)
+    assert_equal '!!!YYBBUURR !!!SSII !!!!!NNUUFF', synth.play('Ruby is fun!')
+
   end
 end
